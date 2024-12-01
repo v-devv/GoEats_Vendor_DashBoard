a04470d4cbc17d424d44c5d07fa1b1408c12abb7
Dev <arjundev0125@gmail.com>
2024-12-01 22:51:41 +0530

    change to live

--- src/vendorDashboard/data/ApiPath.js
+++ src/vendorDashboard/data/ApiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000";
-
-/* export const API_URL = "https://goeats-backend.onrender.com"; */
+/* export const API_URL = "http://localhost:4000";
+ */
+export const API_URL = "https://goeats-backend.onrender.com";
